60db83c7b8d3eadbe2650fb0249d8beb3bbccf51
cabal82 <fabriziogallici@gmail.com>
2019-10-21 15:00:01 +0200

    inizio es dadi

---
+++ js-dadi/assets/js/main.js
@@ -0,0 +1,8 @@
+//Gioco dei dadi, chi fa di più vince
+
+//creo due variabili per il lancio random dei dadi con risultato da 1 a 6
+
+var rndNum1 = Math.floor(Math.random() * 6) + 1;
+var rndNum2 = Math.floor(Math.random() * 6) + 1;
+
+//confronto per determinare il numero più alto e quindi il vincitore
